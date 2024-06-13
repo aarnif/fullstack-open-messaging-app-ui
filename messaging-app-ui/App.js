ae3e34660e5540dc1fa7456c0067becf7fd1b211
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-13 16:39:00 +0300

    Add authentication to apollo server

--- messaging-app-ui/App.js
+++ messaging-app-ui/App.js
@@ -3,17 +3,22 @@ import Main from "./src/components/Main";
 import { NativeRouter } from "react-router-native";
 import { ApolloProvider } from "@apollo/client";
 import createApolloClient from "./src/utils/apolloClient";
+import AuthStorage from "./src/utils/authStorage";
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
 
 import "./styles.css";
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
     </>
--- messaging-app-ui/src/utils/apolloClient.js
+++ messaging-app-ui/src/utils/apolloClient.js
@@ -1,8 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const createApolloClient = () => {
+const httpLink = createHttpLink({
+  uri: "http://84.253.241.99:4000/",
+});
+
+const createApolloClient = (authStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : "",
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers,
+      };
+    }
+  });
   return new ApolloClient({
-    uri: "http://87.92.93.239:4000/",
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 };
