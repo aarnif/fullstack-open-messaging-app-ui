5f04463957b09ec48c1e9a6644a74abe9b8279bd
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-18 09:01:37 +0300

    Create hook for menu animation

--- messaging-app-ui/src/components/Menu/index.jsx
+++ messaging-app-ui/src/components/Menu/index.jsx
@@ -1,29 +1,18 @@
 import MenuButton from "./MenuButton";
 import MenuItems from "./MenuItems";
 import useAuthStorage from "../../hooks/useAuthStorage";
+import useMenuAnimation from "../../hooks/useMenuAnimation";
 
 import { useApolloClient } from "@apollo/client";
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-native";
 import { View } from "react-native";
-import {
-  useChain,
-  useSpring,
-  useSpringRef,
-  useTransition,
-  animated,
-} from "@react-spring/native";
+import { animated } from "@react-spring/native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-const reactSpringAnimationDuration = 0.3;
 const iconSize = 32;
 const iconColor = "white";
 
 const Menu = () => {
-  const authStorage = useAuthStorage();
-  const navigate = useNavigate();
-  const client = useApolloClient();
-
   const menuItemsData = [
     {
       id: "1",
@@ -98,65 +87,17 @@ const Menu = () => {
     },
   ];
 
-  const [showMenu, setShowMenu] = useState(false);
-  const [isFirstRender, setIsFirstRender] = useState(true);
+  const authStorage = useAuthStorage();
+  const navigate = useNavigate();
+  const client = useApolloClient();
 
-  const springsOverlayRef = useSpringRef();
-  const springsOverlay = useSpring({
-    ref: springsOverlayRef,
-    from: {
-      translateX: !isFirstRender ? -1000 : showMenu ? -1000 : 0,
-      opacity: !isFirstRender ? 0 : showMenu ? 0 : 1,
-    },
-    to: {
-      translateX: showMenu ? 0 : -1000,
-      opacity: showMenu ? 1 : 0,
-    },
-    immediate: isFirstRender,
-  });
-
-  useEffect(() => {
-    setIsFirstRender(false);
-  }, []);
-
-  const springsMenuButtonRef = useSpringRef();
-  const springsMenuButton = useSpring({
-    ref: springsMenuButtonRef,
-    from: {
-      rotate: !isFirstRender ? "0deg" : showMenu ? "0deg" : "45deg",
-      backgroundColor: !isFirstRender
-        ? "#16a34a"
-        : showMenu
-        ? "#16a34a"
-        : "rgba(220, 38, 38, 0.5)",
-      borderColor: !isFirstRender
-        ? "#16a34a"
-        : showMenu
-        ? "#16a34a"
-        : "#ef4444",
-    },
-    to: {
-      rotate: showMenu ? "45deg" : "0deg",
-      backgroundColor: showMenu ? "rgba(220, 38, 38, 0.5)" : "#16a34a",
-      borderColor: showMenu ? "#ef4444" : "#16a34a",
-    },
-  });
-
-  const transRef = useSpringRef();
-  const transitions = useTransition(showMenu ? menuItemsData : [], {
-    ref: transRef,
-    trail: 300 / menuItemsData.length,
-    from: { opacity: 0 },
-    enter: { opacity: 1 },
-    leave: { opacity: 0 },
-  });
-
-  useChain(
-    showMenu
-      ? [springsOverlayRef, springsMenuButtonRef, transRef]
-      : [transRef, springsMenuButtonRef, springsOverlayRef],
-    [0, reactSpringAnimationDuration, reactSpringAnimationDuration * 2]
-  );
+  const [
+    springsOverlay,
+    springsMenuButton,
+    transitions,
+    showMenu,
+    setShowMenu,
+  ] = useMenuAnimation(menuItemsData);
 
   return (
     <>
---
+++ messaging-app-ui/src/hooks/useMenuAnimation.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+
+import {
+  useChain,
+  useSpring,
+  useSpringRef,
+  useTransition,
+  animated,
+} from "@react-spring/native";
+
+const reactSpringAnimationDuration = 0.3;
+
+const useMenuAnimation = (menuItemsData) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  const springsOverlayRef = useSpringRef();
+  const springsOverlay = useSpring({
+    ref: springsOverlayRef,
+    from: {
+      translateX: !isFirstRender ? -1000 : showMenu ? -1000 : 0,
+      opacity: !isFirstRender ? 0 : showMenu ? 0 : 1,
+    },
+    to: {
+      translateX: showMenu ? 0 : -1000,
+      opacity: showMenu ? 1 : 0,
+    },
+    immediate: isFirstRender,
+  });
+
+  useEffect(() => {
+    setIsFirstRender(false);
+  }, []);
+
+  const springsMenuButtonRef = useSpringRef();
+  const springsMenuButton = useSpring({
+    ref: springsMenuButtonRef,
+    from: {
+      rotate: !isFirstRender ? "0deg" : showMenu ? "0deg" : "45deg",
+      backgroundColor: !isFirstRender
+        ? "#16a34a"
+        : showMenu
+        ? "#16a34a"
+        : "rgba(220, 38, 38, 0.5)",
+      borderColor: !isFirstRender
+        ? "#16a34a"
+        : showMenu
+        ? "#16a34a"
+        : "#ef4444",
+    },
+    to: {
+      rotate: showMenu ? "45deg" : "0deg",
+      backgroundColor: showMenu ? "rgba(220, 38, 38, 0.5)" : "#16a34a",
+      borderColor: showMenu ? "#ef4444" : "#16a34a",
+    },
+  });
+
+  const transRef = useSpringRef();
+  const transitions = useTransition(showMenu ? menuItemsData : [], {
+    ref: transRef,
+    trail: 300 / menuItemsData.length,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  useChain(
+    showMenu
+      ? [springsOverlayRef, springsMenuButtonRef, transRef]
+      : [transRef, springsMenuButtonRef, springsOverlayRef],
+    [0, reactSpringAnimationDuration, reactSpringAnimationDuration * 2]
+  );
+
+  return [
+    springsOverlay,
+    springsMenuButton,
+    transitions,
+    showMenu,
+    setShowMenu,
+  ];
+};
+
+export default useMenuAnimation;
