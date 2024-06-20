096ad08ab7f273269b2d8a99482ed62c7e35ae9a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-20 10:41:50 +0300

    Add configuration for subscriptions

--- messaging-app-ui/src/utils/apolloClient.js
+++ messaging-app-ui/src/utils/apolloClient.js
@@ -1,10 +1,25 @@
 import Constants from "expo-constants";
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  split,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
 
 const httpLink = createHttpLink({
   uri: Constants.expoConfig.extra.apolloUri,
 });
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000",
+  })
+);
 
 const createApolloClient = (authStorage) => {
   const authLink = setContext(async (_, { headers }) => {
@@ -23,8 +38,21 @@ const createApolloClient = (authStorage) => {
       };
     }
   });
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    authLink.concat(httpLink)
+  );
+
   return new ApolloClient({
-    link: authLink.concat(httpLink),
+    link: splitLink,
     cache: new InMemoryCache(),
   });
 };
