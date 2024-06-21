48218608e5b2fc64e17f074f6766092adc8174dd
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-21 10:19:00 +0300

    Add subscription for new message in chats view

--- messaging-app-ui/src/components/Chats/index.jsx
+++ messaging-app-ui/src/components/Chats/index.jsx
@@ -1,4 +1,5 @@
 import { GET_CHATS_BY_USER } from "../../graphql/queries";
+import { NEW_MESSAGE_ADDED } from "../../graphql/subscriptions";
 import Header from "../Header";
 import SearchBar from "../SearchBar";
 import ChatItem from "./ChatItem";
@@ -7,7 +8,7 @@ import Menu from "../Menu";
 
 import { useState } from "react";
 import { View, Text, FlatList } from "react-native";
-import { useQuery } from "@apollo/client";
+import { useQuery, useSubscription, useApolloClient } from "@apollo/client";
 import { useDebounce } from "use-debounce";
 
 const ChatsHeader = ({ searchByTitle, handleChange }) => {
@@ -47,17 +48,41 @@ const ChatsList = ({ data }) => {
 };
 
 const Chats = ({ user }) => {
+  const client = useApolloClient();
   const [searchByTitle, setSearchByTitle] = useState("");
-  const [debouncedSearchByTitle, setDebouncedSearchByTitle] = useDebounce(
-    searchByTitle,
-    500
-  );
+  const [debouncedSearchByTitle] = useDebounce(searchByTitle, 500);
   const { data, loading } = useQuery(GET_CHATS_BY_USER, {
     variables: {
       userId: user.id,
       searchByTitle: debouncedSearchByTitle,
     },
     fetchPolicy: "cache-and-network",
+  });
+
+  useSubscription(NEW_MESSAGE_ADDED, {
+    onData: ({ data }) => {
+      const updatedChat = data.data.messageToChatAdded;
+      client.cache.updateQuery(
+        {
+          query: GET_CHATS_BY_USER,
+          variables: { userId: user.id, searchByTitle: searchByTitle },
+        },
+        ({ allChatsByUser }) => {
+          return {
+            allChatsByUser: allChatsByUser
+              .map((chat) => {
+                return chat.id === updatedChat.id ? updatedChat : chat;
+              })
+              .sort((a, b) => {
+                return (
+                  new Date(b.messages[0].createdAt) -
+                  new Date(a.messages[0].createdAt)
+                );
+              }),
+          };
+        }
+      );
+    },
   });
 
   // console.log("Chats data:", data);
