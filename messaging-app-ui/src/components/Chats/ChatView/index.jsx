e3f399948216e699c2ca8389709d3a05a3d50b26
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-21 10:10:05 +0300

    Add subscription for new message in single chat view

--- messaging-app-ui/src/components/Chats/ChatView/index.jsx
+++ messaging-app-ui/src/components/Chats/ChatView/index.jsx
@@ -1,12 +1,14 @@
-import { GET_CHAT_BY_ID } from "../../../graphql/queries";
+import { GET_CHAT_BY_ID, GET_CHATS_BY_USER } from "../../../graphql/queries";
 import LoadingIcon from "../../LoadingIcon";
 import ChatHeader from "./ChatHeader";
 import Messages from "./Messages";
+import { NEW_MESSAGE_ADDED } from "../../../graphql/subscriptions";
 
-import { useQuery } from "@apollo/client";
+import { useQuery, useSubscription, useApolloClient } from "@apollo/client";
 import { useMatch } from "react-router-native";
 
 const ChatView = ({ user }) => {
+  const client = useApolloClient();
   const match = useMatch("/chats/:chatId").params;
   const { data, loading } = useQuery(GET_CHAT_BY_ID, {
     variables: {
@@ -15,8 +17,21 @@ const ChatView = ({ user }) => {
     fetchPolicy: "cache-and-network",
   });
 
-  // console.log("Match:", match.chatId);
-  // console.log("Chat view data:", data);
+  useSubscription(NEW_MESSAGE_ADDED, {
+    onData: ({ data }) => {
+      const updatedChat = data.data.messageToChatAdded;
+      client.cache.updateQuery(
+        { query: GET_CHAT_BY_ID, variables: { chatId: match.chatId } },
+        ({ findChatById }) => {
+          return {
+            findChatById: {
+              ...findChatById,
+            },
+          };
+        }
+      );
+    },
+  });
 
   if (loading) {
     return <LoadingIcon />;
---
+++ messaging-app-ui/src/graphql/subscriptions.js
@@ -0,0 +1,13 @@
+import { CHAT_DETAILS } from "./queries";
+
+import { gql } from "@apollo/client";
+
+export const NEW_MESSAGE_ADDED = gql`
+  subscription MessageToChatAdded {
+    messageToChatAdded {
+      ...ChatDetails
+    }
+  }
+
+  ${CHAT_DETAILS}
+`;
