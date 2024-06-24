6fb4f81453af2e056dc957484ee42521fd898fd1
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-24 16:37:48 +0300

    Change searchbar icon

--- messaging-app-ui/src/components/SearchBar.jsx
+++ messaging-app-ui/src/components/SearchBar.jsx
@@ -1,12 +1,11 @@
-import { View, Text, TextInput } from "react-native";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { View, TextInput } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 const SearchBar = ({ placeholder, searchByTitle, handleChange }) => {
   return (
     <View className="mx-4 my-2">
       <View className="w-full flex-grow flex-row max-h-[50px] p-2 rounded-lg bg-slate-200 text-slate-800">
-        <FontAwesomeIcon color={"#475569"} icon={faMagnifyingGlass} />
+        <MaterialCommunityIcons size={24} color={"#475569"} name="magnify" />
         <TextInput
           className="w-full pl-2"
           placeholder={placeholder}
