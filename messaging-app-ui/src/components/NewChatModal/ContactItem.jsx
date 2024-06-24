8699237555c66c77bf43530aa378361e6bf3252e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-24 11:05:10 +0300

    Add animation when click contact

--- messaging-app-ui/src/components/NewChatModal/ContactItem.jsx
+++ messaging-app-ui/src/components/NewChatModal/ContactItem.jsx
@@ -2,9 +2,16 @@ import { Pressable, View, Text, Image, TextInput } from "react-native";
 import baseUrl from "../../../baseUrl";
 
 import { useState } from "react";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useSpring, animated } from "@react-spring/native";
 
 const ContactItem = ({ user, item, setChosenUsersIds }) => {
   const [choose, setChoose] = useState(false);
+  const [springs, api] = useSpring(() => ({
+    from: {
+      scale: choose ? 0.95 : 1,
+    },
+  }));
 
   const handlePress = () => {
     console.log("Contact pressed:", item.id);
@@ -14,11 +21,24 @@ const ContactItem = ({ user, item, setChosenUsersIds }) => {
       setChosenUsersIds((prev) => [...prev, item.id]);
     }
     setChoose(!choose);
+    api.start({
+      from: {
+        scale: choose ? 0.95 : 1,
+      },
+      to: {
+        scale: choose ? 1 : 0.95,
+      },
+    });
   };
 
   return (
     <Pressable onPress={handlePress}>
-      <View className="flex flex-row items-center my-2 mx-4">
+      <animated.View
+        style={{
+          transform: [{ scale: springs.scale }],
+        }}
+        className="flex flex-row items-center my-2 mx-4"
+      >
         <View className="mr-4">
           <Image
             source={{
@@ -38,10 +58,17 @@ const ContactItem = ({ user, item, setChosenUsersIds }) => {
           </View>
           <Text className="text-sm text-slate-700">{item.about}</Text>
         </View>
-        <View>
-          {choose && <Text className="text-sm text-slate-700">Chosen</Text>}
-        </View>
-      </View>
+
+        {choose ? (
+          <View className="mr-4 w-6 h-6 flex justify-center items-center border border-green-600 bg-green-600 rounded-full">
+            <MaterialCommunityIcons name="check" size={20} color="white" />
+          </View>
+        ) : (
+          <View className="mr-4 w-6 h-6 flex justify-center items-center border border-slate-300 rounded-full">
+            <MaterialCommunityIcons name="check" size={20} color="white" />
+          </View>
+        )}
+      </animated.View>
     </Pressable>
   );
 };
