47a504f29698786b686e00109b0cd22138aec45e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-07-11 09:45:44 +0300

    Add useChangeImage-hook

---
+++ messaging-app-ui/src/hooks/useChangeImage.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import * as ImagePicker from "expo-image-picker";
+import { useCameraPermissions } from "expo-camera";
+
+const useChangeImage = (currentImage) => {
+  const [image, setImage] = useState(currentImage);
+  const [base64Image, setBase64Image] = useState(null); // Base64 image required in uploading image to server
+
+  const [permission, requestPermission] = useCameraPermissions();
+  const chooseImageFromCamera = async () => {
+    if (!permission.granted) {
+      console.log("Request camera permission");
+      const { granted } = await requestPermission();
+      if (!granted) {
+        console.log("Permission not granted");
+        return;
+      }
+    }
+
+    console.log("Permission granted");
+
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setBase64Image(result.assets[0].base64);
+    }
+  };
+
+  const chooseImageFromFiles = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      base64: true,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setBase64Image(result.assets[0].base64);
+    }
+  };
+
+  return { image, base64Image, chooseImageFromCamera, chooseImageFromFiles };
+};
+
+export default useChangeImage;
