a85bd48050b12292b624854f8d845464221acd6e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-07-20 10:00:37 +0300

    Add toggle dark mode button

--- messaging-app-ui/src/components/Settings/index.jsx
+++ messaging-app-ui/src/components/Settings/index.jsx
@@ -1,6 +1,28 @@
 import Menu from "../Menu";
 
-import { View, Text } from "react-native";
+import { useColorScheme } from "nativewind";
+import { Pressable, View, Text } from "react-native";
+
+const EnableDarkMode = () => {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <View className="mx-4 p-2 flex flex-row justify-between items-center rounded-lg bg-slate-200">
+      <Text className="text-md font-semibold text-slate-700">Dark Mode</Text>
+      <Pressable onPress={toggleColorScheme}>
+        <View
+          style={{
+            backgroundColor: colorScheme === "dark" ? "#22c55e" : "#94a3b8",
+            alignItems: colorScheme === "dark" ? "flex-end" : "flex-start",
+          }}
+          className="w-16 h-9 flex justify-center rounded-full bg-slate-400"
+        >
+          <View className="m-[1px] w-8 h-8 rounded-full bg-slate-100"></View>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
 
 const Settings = ({ user }) => {
   return (
@@ -15,7 +37,7 @@ const Settings = ({ user }) => {
         </View>
       </View>
       <Text className="text-2xl font-bold mt-4 mx-4 mb-2">Settings</Text>
-
+      <EnableDarkMode />
       {user && <Menu />}
     </>
   );
