06f6dc00fc770881d246653daed87935ca877acc
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-07-28 16:24:09 +0300

    Extract contact item to ContactCard-component

---
+++ messaging-app-ui/src/components/ContactCard.jsx
@@ -0,0 +1,31 @@
+import { View, Text, Image } from "react-native";
+
+const ContactCard = ({ item }) => {
+  return (
+    <>
+      <View className="mr-4">
+        <Image
+          source={{
+            uri: item.profilePicture.thumbnail,
+          }}
+          style={{ width: 48, height: 48, borderRadius: 9999 }}
+        />
+      </View>
+      <View className="flex-1">
+        <View className="flex flex-row">
+          <Text className="text-md text-slate-700 font-bold dark:text-slate-100">
+            {item.name}
+          </Text>
+          <Text className="ml-1 text-md text-slate-500 font-bold dark:text-slate-300">
+            @{item.username}
+          </Text>
+        </View>
+        <Text className="text-sm text-slate-700 dark:text-slate-100">
+          {item.about}
+        </Text>
+      </View>
+    </>
+  );
+};
+
+export default ContactCard;
