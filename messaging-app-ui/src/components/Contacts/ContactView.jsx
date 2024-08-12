cd097c52e363ab4df6387e109496200ed5c09921
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-12 12:05:22 +0300

    Update Contact View

--- messaging-app-ui/src/components/Contacts/ContactView.jsx
+++ messaging-app-ui/src/components/Contacts/ContactView.jsx
@@ -213,50 +213,55 @@ const ContactView = ({ user }) => {
         <Text className="mt-4 text-base text-slate-700 dark:text-slate-100">
           {contact.about}
         </Text>
-      </View>
 
-      <View className="w-full p-4 flex-1 justify-end items-start bg-white dark:bg-slate-700">
-        {haveContactBlockedYou ? (
-          <View className="w-full flex-grow max-h-[60px] flex flex-row justify-center items-center bg-red-400 p-2 rounded-xl">
-            <Text className="text-xl text-slate-200 font-bold">
-              This contact has blocked you!
-            </Text>
-          </View>
-        ) : isBlocked ? (
-          <View className="w-full flex-grow max-h-[60px] flex flex-row justify-center items-center bg-red-400 p-2 rounded-xl">
-            <Text className="text-xl text-slate-200 font-bold">
+        {isBlocked && (
+          <View className="mt-2 w-full flex-grow max-h-[60px] flex flex-row justify-center items-center p-2 rounded-xl">
+            <Text className="text-xl text-red-600 font-bold">
               You have blocked this contact!
             </Text>
           </View>
-        ) : (
-          <Pressable
-            onPress={handleChat}
-            className="w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-green-400 bg-green-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-200">Chat</Text>
-          </Pressable>
         )}
+        {haveContactBlockedYou && (
+          <View className="mt-2 w-full flex-grow max-h-[60px] flex flex-row justify-center items-center p-2 rounded-xl">
+            <Text className="text-xl text-red-600 font-bold">
+              This contact has blocked you!
+            </Text>
+          </View>
+        )}
+      </View>
 
-        {isBlocked ? (
-          <Pressable
-            onPress={handleBlockContact}
-            className="w-full flex-grow max-h-[60px] mt-2 p-2 flex justify-center items-center border-2 border-yellow-400 bg-yellow-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-400">Unblock</Text>
-          </Pressable>
-        ) : (
-          <Pressable
-            onPress={handleBlockContact}
-            className="w-full flex-grow max-h-[60px] mt-2 p-2 flex justify-center items-center border-2 border-yellow-400 bg-yellow-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-200">Block</Text>
-          </Pressable>
-        )}
+      <View className="w-full p-4 flex-1 justify-end items-start bg-white dark:bg-slate-700">
+        <Pressable
+          onPress={handleChat}
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
+        >
+          <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+            Chat
+          </Text>
+        </Pressable>
+
+        <Pressable
+          onPress={handleBlockContact}
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
+        >
+          {isBlocked ? (
+            <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+              Unblock Contact
+            </Text>
+          ) : (
+            <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+              Block Contact
+            </Text>
+          )}
+        </Pressable>
         <Pressable
           onPress={handleRemoveContact}
-          className="w-full flex-grow max-h-[60px] mt-2 p-2 flex justify-center items-center border-2 border-red-400 bg-red-400 rounded-xl"
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
         >
-          <Text className="text-xl font-bold text-slate-200">
+          <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
             Remove Contact
           </Text>
         </Pressable>
