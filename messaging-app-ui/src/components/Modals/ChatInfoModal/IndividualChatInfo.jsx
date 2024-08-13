b7da7bc75175dbe69a37f1554139bf650165419a
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-13 09:46:47 +0300

    Update IndividualChatInfo

--- messaging-app-ui/src/components/Modals/ChatInfoModal/IndividualChatInfo.jsx
+++ messaging-app-ui/src/components/Modals/ChatInfoModal/IndividualChatInfo.jsx
@@ -1,4 +1,9 @@
-import { BLOCK_OR_UNBLOCK_CONTACT } from "../../../graphql/mutations";
+import {
+  BLOCK_OR_UNBLOCK_CONTACT,
+  REMOVE_CONTACT,
+} from "../../../graphql/mutations";
+import useNotifyMessage from "../../../hooks/useNotifyMessage";
+import confirmAlert from "../../confirmAlert";
 
 import { useState } from "react";
 import { View, Text, Pressable, Image } from "react-native";
@@ -15,6 +20,7 @@ const IndividualChatInfo = ({
     (participant) => participant.id !== user.id
   )[0];
 
+  const notifyMessage = useNotifyMessage();
   const [contact, setContact] = useState(getAnotherParticipant);
   const [isBlocked, setIsBlocked] = useState(
     user.blockedContacts.includes(getAnotherParticipant.id)
@@ -23,6 +29,13 @@ const IndividualChatInfo = ({
   const [blockOrUnBlockContact] = useMutation(BLOCK_OR_UNBLOCK_CONTACT, {
     onError: (error) => {
       console.log("Error blocking contact mutation:");
+      console.log(error.graphQLErrors[0].message);
+    },
+  });
+
+  const [removeContact] = useMutation(REMOVE_CONTACT, {
+    onError: (error) => {
+      console.log("Error removing contact mutation:");
       console.log(error.graphQLErrors[0].message);
     },
   });
@@ -48,12 +61,40 @@ const IndividualChatInfo = ({
 
       if (isContactBlocked) {
         console.log("Blocked contact:", contact.id);
+        notifyMessage.show({
+          content: "Contact blocked!",
+          isError: true,
+        });
       } else {
         console.log("Unblocked contact:", contact.id);
+        notifyMessage.show({
+          content: "Contact unblocked!",
+          isError: false,
+        });
       }
       setIsBlocked(isContactBlocked);
     } catch (error) {
       console.log("Error blocking contact:", error);
+      console.log(error.message);
+    }
+  };
+
+  const handleRemoveContact = () => {
+    console.log("Press remove contact button!");
+
+    try {
+      const { data, error } = removeContact({
+        variables: {
+          contactId: contact.id,
+        },
+      });
+      setShowChatInfoModal(false);
+      notifyMessage.show({
+        content: "Contact removed!",
+        isError: false,
+      });
+    } catch (error) {
+      console.log("Error removing contact:", error);
       console.log(error.message);
     }
   };
@@ -81,44 +122,71 @@ const IndividualChatInfo = ({
         <Text className="mt-4 text-base text-slate-700 dark:text-slate-100">
           {contact.about}
         </Text>
-      </View>
-      <View className="w-full p-4 flex-1 justify-end items-start bg-white dark:bg-slate-700">
-        {haveContactBlockedYou ? (
-          <View className="w-full flex-grow max-h-[60px] flex flex-row justify-center items-center bg-red-400 p-2 rounded-xl">
-            <Text className="text-xl text-slate-200 font-bold">
-              This contact has blocked you!
-            </Text>
-          </View>
-        ) : isBlocked ? (
-          <View className="w-full flex-grow max-h-[60px] flex flex-row justify-center items-center bg-red-400 p-2 rounded-xl">
-            <Text className="text-xl text-slate-200 font-bold">
+
+        {isBlocked && (
+          <View className="mt-2 w-full flex-grow max-h-[60px] flex flex-row justify-center items-center p-2 rounded-xl">
+            <Text className="text-xl text-red-600 font-bold">
               You have blocked this contact!
             </Text>
           </View>
-        ) : (
-          <Pressable
-            onPress={handleChat}
-            className="w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-green-400 bg-green-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-200">Chat</Text>
-          </Pressable>
         )}
+        {haveContactBlockedYou && (
+          <View className="mt-2 w-full flex-grow max-h-[60px] flex flex-row justify-center items-center p-2 rounded-xl">
+            <Text className="text-xl text-red-600 font-bold">
+              This contact has blocked you!
+            </Text>
+          </View>
+        )}
+      </View>
+      <View className="w-full p-4 flex-1 justify-end items-start bg-white dark:bg-slate-700">
+        <Pressable
+          onPress={handleChat}
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
+        >
+          <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+            Chat
+          </Text>
+        </Pressable>
 
-        {isBlocked ? (
-          <Pressable
-            onPress={handleBlockContact}
-            className="w-full flex-grow max-h-[60px] mt-2 p-2 flex justify-center items-center border-2 border-yellow-400 bg-yellow-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-400">Unblock</Text>
-          </Pressable>
-        ) : (
-          <Pressable
-            onPress={handleBlockContact}
-            className="w-full flex-grow max-h-[60px] mt-2 p-2 flex justify-center items-center border-2 border-red-400 bg-red-400 rounded-xl"
-          >
-            <Text className="text-xl font-bold text-slate-200">Block</Text>
-          </Pressable>
-        )}
+        <Pressable
+          onPress={() =>
+            confirmAlert(
+              isBlocked ? "Unblock Contact?" : "Block Contact?",
+              isBlocked
+                ? "Are you sure you want to unblock this contact?"
+                : "Are you sure you want to block this contact?",
+              handleBlockContact
+            )
+          }
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
+        >
+          {isBlocked ? (
+            <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+              Unblock Contact
+            </Text>
+          ) : (
+            <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+              Block Contact
+            </Text>
+          )}
+        </Pressable>
+        <Pressable
+          onPress={() =>
+            confirmAlert(
+              "Remove Contact?",
+              "Are you sure you want to remove this contact?",
+              handleRemoveContact
+            )
+          }
+          className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl 
+            dark:border-slate-500 dark:bg-slate-500"
+        >
+          <Text className="text-xl font-bold text-slate-700 dark:text-slate-200">
+            Remove Contact
+          </Text>
+        </Pressable>
       </View>
     </>
   );
