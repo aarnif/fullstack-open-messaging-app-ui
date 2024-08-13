8145f9f96ddea911ee2ec70593f2d1ce7883ddbe
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-13 09:23:21 +0300

    Update NotifyMessage-styles

--- messaging-app-ui/src/components/NotifyMessage.jsx
+++ messaging-app-ui/src/components/NotifyMessage.jsx
@@ -24,14 +24,14 @@ const NotifyMessage = () => {
             height: "10%",
             marginTop: "auto",
             backgroundColor: content.isError
-              ? "rgba(220, 38, 38, 0.5)"
-              : "rgba(22, 163, 74, 0.5)",
+              ? "rgba(153, 27, 27, 0.8)"
+              : "rgba(22, 101, 52, 0.8)",
           }}
           className="w-full flex justify-center items-center"
         >
           <View className="w-full p-4 rounded-xl flex justify-center items-center">
             <Text
-              style={{ color: content.isError ? "#dc2626" : "#16a34a" }}
+              style={{ color: content.isError ? "#ef4444" : "#22c55e" }}
               className="text-2xl font-bold"
             >
               {content.content}
